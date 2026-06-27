3716844bdb34bb22f0f6dabdb5545aa57a5b4c69
Olamide Treasure Oluwalade <112403968+treasureola@users.noreply.github.com>
2026-06-27 12:37:07 -0400

    Time: 0 ms (100.00%) | Memory: 12.7 MB (33.60%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = nums
+        ans.extend(nums)
+        
+        return ans
+
+        
